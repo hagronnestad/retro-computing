5931fe9ecbb7878d3da88b35e98f21b3f978e5c5
Hein Andre Grønnestad <hag@haugnett.no>
2019-08-23 00:28:14 +0200

    OpCodeAttribute for decoration and maybe reflection, not sure if I'll use it yet

---
+++ Cpu6502/OpCodeAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Cpu6502 {
+    public class OpCodeAttribute : Attribute {
+
+        public string Name { get; set; }
+        public byte Code { get; set; }
+        public ushort Length { get; set; }
+        public ushort Cycles { get; set; }
+        public bool AddCycleIfBoundaryCrossed { get; set; }
+
+        public string Description { get; set; }
+    }
+}
