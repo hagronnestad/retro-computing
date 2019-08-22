7f888ddfb61601d460e0e392a4eed665ada10265
Hein Andre Grønnestad <hag@haugnett.no>
2019-08-23 00:26:24 +0200

    Status register flags enum

---
+++ Cpu6502/StatusRegisterFlag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Cpu6502 {
+    [Flags]
+    public enum ProcessorStatusFlags : byte {
+        Carry = 1,
+        Zero = 2,
+        IrqDisable = 4,
+        DecimalMode = 8,
+        BreakCommand = 16,
+        Reserved = 32,
+        Overflow = 64,
+        Negative = 128
+    }
+}
