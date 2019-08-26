e81801ecabbee04d7ab3a1f36a42e3962f2cf376
Hein Andre Grønnestad <hag@haugnett.no>
2019-08-26 19:49:50 +0200

    Added some stats output in Program.cs

--- Cpu6502Emulator/Program.cs
+++ Cpu6502Emulator/Program.cs
@@ -1,9 +1,24 @@
 ﻿using System;
+using System.Linq;
 
 namespace Cpu6502 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("6502.NET\n");
+
+            Console.WriteLine("Creating CPU instance and caching opcodes...\n");
+            var p = new Cpu6502();
+
+            Console.WriteLine($"Implemented OpCodes: {p.OpCodes.Select(x => x.Name).Distinct().Count()} / 56");
+            Console.WriteLine($"Implemented OpCodes including different adressing modes: {p.OpCodes.Count()} / 151");
+            Console.WriteLine("Press any key to continue...\n");
+            Console.ReadKey();
+
+            foreach (var item in p.OpCodes) {
+                Console.WriteLine($"Name = {item.Name}, Code = 0x{item.Code:X} AddressingMode = {item.AddressingMode}");
+            }
+
+            Console.ReadKey();
         }
     }
 }
