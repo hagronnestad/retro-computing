7c9856b7050177788ed3a31a08ad3bf426c601e7
Hein Andre Grønnestad <hag@haugnett.no>
2019-08-26 19:45:11 +0200

    Added OpCodeDefinition class

---
+++ Cpu6502/OpCodeDefinition.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Cpu6502 {
+    public class OpCodeDefinition {
+        public byte Code { get; set; }
+        public string Name { get; set; }
+        public ushort Length { get; set; }
+        public ushort Cycles { get; set; }
+        public bool AddCycleIfBoundaryCrossed { get; set; }
+
+        public AddressingMode AddressingMode { get; set; }
+
+        public string Description { get; set; }
+
+        public Action<object[]> Action { get; set; }
+
+        public OpCodeDefinition() {
+
+        }
+
+        public static OpCodeDefinition FromOpCodeAttribute(Action<object[]> action, OpCodeAttribute a) {
+            return new OpCodeDefinition {
+                Action = action,
+                AddCycleIfBoundaryCrossed = a.AddCycleIfBoundaryCrossed,
+                AddressingMode = a.AddressingMode,
+                Code = a.Code,
+                Cycles = a.Cycles,
+                Description = a.Description,
+                Length = a.Length,
+                Name = a.Name
+            };
+        }
+    }
+}
