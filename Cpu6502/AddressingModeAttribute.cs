45e4eb20dc0049d376c0af0e91b28d913c71588f
Hein Andre Grønnestad <hag@haugnett.no>
2019-08-28 01:27:08 +0200

    Added AddressingModeAttribute

---
+++ Cpu6502/AddressingModeAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Cpu6502 {
+
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class AddressingModeAttribute : Attribute {
+        public AddressingMode AddressingMode { get; set; }
+    }
+
+}
