7f7bb661d8680b4f60329ebcc09b93dd84921df8
Hein Andre Grønnestad <hag@haugnett.no>
2019-09-29 03:43:39 +0200

    #8 Added WatchItem.cs

---
+++ Debugger/WatchItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Debugger {
+
+    public class WatchItem {
+
+        public int Address { get; set; }
+        public string ValueHex { get; set; }
+        public string ValueDecimal { get; set; }
+        public string Description { get; set; }
+
+    }
+
+}
