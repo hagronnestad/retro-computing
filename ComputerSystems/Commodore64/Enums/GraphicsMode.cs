32facd3b071d531db6a04681ad332c61ad302eb2
Hein Andre Grønnestad <hag@haugnett.no>
2020-03-29 20:57:04 +0200

    #2 Added GraphicsMode enum

---
+++ ComputerSystems/Commodore64/Enums/GraphicsMode.cs
@@ -0,0 +1,17 @@
+﻿namespace Commodore64.Enums {
+
+    /// <summary>
+    /// GraphicsMode as defined by MCM (bit 4 in $D016), BMM (bit 5 in $D011) and ECM (bit 6 in $D011)
+    /// Source: https://www.c64-wiki.com/wiki/Graphics_Modes#Implementation
+    /// </summary>
+    public enum GraphicsMode {
+        StandardCharacterMode = 0b000,
+        MultiColorCharacterMode = 0b001,
+        StandardBitmapMode = 0b010,
+        MulticolorBitmapMode = 0b011,
+        ExtendedBackgroundColorMode = 0b100,
+        UNOFFICIAL_ExtendedBackgroundColorMulticolorCharacterMode = 0b101,
+        UNOFFICIAL_ExtendedBackgroundColorStandardBitmapMode = 0b110,
+        UNOFFICIAL_ExtendedBackgroundColorMulticolorBitmapMode = 0b111
+    }
+}
