cfd57b33bbe5ef8db0944b80a520bd964ff17acc
Hein Andre Grønnestad <hag@haugnett.no>
2020-04-09 23:32:53 +0200

    #13 More CIA2 work

--- ComputerSystems/Commodore64/Cia/Cia2.cs
+++ ComputerSystems/Commodore64/Cia/Cia2.cs
@@ -1,4 +1,6 @@
 ﻿using Commodore64.Cia.Enums;
+using Extensions.Byte;
+using Extensions.Enums;
 
 namespace Commodore64.Cia {
     
@@ -13,23 +15,72 @@ namespace Commodore64.Cia {
 
         public byte[] _registers = new byte[0x10];
 
+        public Cia2() {
+            _registers[0] = 0b00000011;
+        }
+
         public byte this[Register index] {
-            get {
+            get { // Read
+                var i = (int)index;
+
                 switch (index) {
 
                     default:
-                        return _registers[(int)index];
+                        return _registers[i];
                 }
             }
-            set {
+            set { // Write
+                var i = (int) index;
+
                 switch (index) {
+                    case Register.R_0x00_PORT_A:
+                        // Apply data direction register
+                        // Only bits which are set in the data direction register can be set in the port register
+
+                        var ddrA = _registers[(byte)Register.R_0x02_PORT_A_DATA_DIRECTION];
+                        
+                        // Should have the same effect as the below code if I'm not mistaken
+                        _registers[i] = (byte)((value & ddrA) | (_registers[i] & ~ddrA));
+
+                        //if (ddrA.IsBitSet(BitFlag.BIT_0)) _registers[i] = _registers[i].SetBit(BitFlag.BIT_0, value.IsBitSet(BitFlag.BIT_0));
+                        //if (ddrA.IsBitSet(BitFlag.BIT_1)) _registers[i] = _registers[i].SetBit(BitFlag.BIT_1, value.IsBitSet(BitFlag.BIT_1));
+                        //if (ddrA.IsBitSet(BitFlag.BIT_2)) _registers[i] = _registers[i].SetBit(BitFlag.BIT_2, value.IsBitSet(BitFlag.BIT_2));
+                        //if (ddrA.IsBitSet(BitFlag.BIT_3)) _registers[i] = _registers[i].SetBit(BitFlag.BIT_3, value.IsBitSet(BitFlag.BIT_3));
+                        //if (ddrA.IsBitSet(BitFlag.BIT_4)) _registers[i] = _registers[i].SetBit(BitFlag.BIT_4, value.IsBitSet(BitFlag.BIT_4));
+                        //if (ddrA.IsBitSet(BitFlag.BIT_5)) _registers[i] = _registers[i].SetBit(BitFlag.BIT_5, value.IsBitSet(BitFlag.BIT_5));
+                        //if (ddrA.IsBitSet(BitFlag.BIT_6)) _registers[i] = _registers[i].SetBit(BitFlag.BIT_6, value.IsBitSet(BitFlag.BIT_6));
+                        //if (ddrA.IsBitSet(BitFlag.BIT_7)) _registers[i] = _registers[i].SetBit(BitFlag.BIT_7, value.IsBitSet(BitFlag.BIT_7));
+                        break;
+
+                    case Register.R_0x01_PORT_B:
+                        // Apply data direction register
+                        // Only bits which are set in the data direction register can be set in the port register
+
+                        var ddrB = _registers[(byte)Register.R_0x03_PORT_B_DATA_DIRECTION];
+
+                        // Should have the same effect as the below code if I'm not mistaken
+                        _registers[i] = (byte)((value & ddrB) | (_registers[i] & ~ddrB));
+
+                        //if (ddrB.IsBitSet(BitFlag.BIT_0)) _registers[i] = _registers[i].SetBit(BitFlag.BIT_0, value.IsBitSet(BitFlag.BIT_0));
+                        //if (ddrB.IsBitSet(BitFlag.BIT_1)) _registers[i] = _registers[i].SetBit(BitFlag.BIT_1, value.IsBitSet(BitFlag.BIT_1));
+                        //if (ddrB.IsBitSet(BitFlag.BIT_2)) _registers[i] = _registers[i].SetBit(BitFlag.BIT_2, value.IsBitSet(BitFlag.BIT_2));
+                        //if (ddrB.IsBitSet(BitFlag.BIT_3)) _registers[i] = _registers[i].SetBit(BitFlag.BIT_3, value.IsBitSet(BitFlag.BIT_3));
+                        //if (ddrB.IsBitSet(BitFlag.BIT_4)) _registers[i] = _registers[i].SetBit(BitFlag.BIT_4, value.IsBitSet(BitFlag.BIT_4));
+                        //if (ddrB.IsBitSet(BitFlag.BIT_5)) _registers[i] = _registers[i].SetBit(BitFlag.BIT_5, value.IsBitSet(BitFlag.BIT_5));
+                        //if (ddrB.IsBitSet(BitFlag.BIT_6)) _registers[i] = _registers[i].SetBit(BitFlag.BIT_6, value.IsBitSet(BitFlag.BIT_6));
+                        //if (ddrB.IsBitSet(BitFlag.BIT_7)) _registers[i] = _registers[i].SetBit(BitFlag.BIT_7, value.IsBitSet(BitFlag.BIT_7));
+                        break;
 
                     default:
-                        _registers[(int)index] = value;
+                        _registers[i] = value;
                         break;
                 }
             }
         }
 
+
+        public void Clock() {
+        
+        }
     }
 }
